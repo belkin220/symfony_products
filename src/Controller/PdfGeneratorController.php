1a7bcdff3c1e7a37c7d2611886b5478a29630902
belkin220 <ixlevante21@yahoo.es>
2023-05-07 20:44:05 +0200

    Update

--- src/Controller/PdfGeneratorController.php
+++ src/Controller/PdfGeneratorController.php
@@ -2,8 +2,9 @@
 
 namespace App\Controller;
 
-use Dompdf\Dompdf;
+
 use App\Entity\Product;
+use App\Service\PdfGenerator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,12 +15,8 @@ class PdfGeneratorController extends AbstractController
     
     #[Route('/pdf/generator', name: 'app_pdf_generator')]
 
-    public function index(EntityManagerInterface $entityManager): Response
-    {
-        $pdf = new Dompdf();
-        $options = new \Dompdf\Options();
-        $canvas = $pdf->getCanvas();
-              
+    public function index(EntityManagerInterface $entityManager)
+    {       
         $data = $entityManager->getRepository(Product::class)->findAll();
         $html =  $this->renderView('pdf_generator/index.html.twig',
          [
@@ -27,19 +24,9 @@ class PdfGeneratorController extends AbstractController
             'date' => date("d/m/Y"),
            
         ]);
-              
-        $pdf->loadHtml($html);
-        $pdf->setPaper('A4', 'landscape');
-        $options->set('isPhpEnabled', true);
-        $options->set('defaultFont','Arial');
-        $options->set('isRemoteEnabled', true);
-        $pdf->setOptions($options);
-        $pdf->render();
-        return new Response (
-            $pdf->stream('resume.pdf', [
-                "Attachment" => false,]),
-            Response::HTTP_OK,
-            ['Content-Type' => 'application/pdf']);
+        $pdf = new PdfGenerator();
+        $pdf->generatePdf($html); 
+        
     }
 
 }
---
+++ src/Service/PdfGenerator.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Service;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+class PdfGenerator extends Dompdf
+{
+    public function generatePdf($html)
+    {
+        $options = new Options(); 
+        $this->loadHtml($html); 
+        $this->setPaper('A4', 'landscape');
+        $options->set('isPhpEnabled', true);
+        $options->set('defaultFont','Arial');
+        $options->set('isRemoteEnabled', true);
+        $this->setOptions($options);
+        $this->render();
+        return 
+            $this->stream('resume.pdf', ["Attachment" => false,]);
+    }
+
+}
